0940244a43b99b32b6d0834ddd96b9a470b956a0
Admin <andrewthomasm73@gmail.com>
2024-11-22 02:49:09 -0500

    better form formatting, calendar link, section spacing, registry page

--- js/rsvpscript.js
+++ js/rsvpscript.js
@@ -1,21 +1,24 @@
 // Valid invite code hashes (replace these with MD5 hashes of actual invite codes)
 const validHashes = [
-    '5d41402abc4b2a76b9719d911017c592', // Example hash for "hello"
-    '098f6bcd4621d373cade4e832627b4f6',  // Example hash for "test"
+    // '5d41402abc4b2a76b9719d911017c592', // Example hash for "hello"
+    // '098f6bcd4621d373cade4e832627b4f6',  // Example hash for "test"
     'c4ca4238a0b923820dcc509a6f75849b'
 ];
 
-function verifyCode(event) {
+async function verifyCode(event) {
     event.preventDefault();
 
     const inviteCodeInput = document.getElementById('invite_code').value.trim();
     const hashedCode = CryptoJS.MD5(inviteCodeInput).toString();
 
+    document.getElementById('rsvp_yes_calendar').style.display = 'none';
+
     if (validHashes.includes(hashedCode)) {
         // Show the additional details form if the invite code is correct
+        document.getElementById('response_message').innerHTML = "<strong>Validating...</strong> One moment please.";
+        await getSheetsApi(inviteCodeInput);
         document.getElementById('rsvpDetailsForm').style.display = 'block';
         document.getElementById('response_message').innerHTML = "<strong>Code validated!</strong> Please provide additional details below.";
-        getSheetsApi(inviteCodeInput);
         document.getElementById('retrieve_invite').style.display = 'none';
     } else {
         // Display an error message if the invite code is incorrect
@@ -26,6 +29,11 @@ function verifyCode(event) {
 
 async function submitRsvp(event) {
     event.preventDefault();
+    
+    if (!document.getElementById('rsvp').value) {
+        document.getElementById('submit_message').innerHTML = '<strong>Error!</strong> Please make an RSVP selection using the buttons above.';
+        return;
+    }
 
     document.getElementById('submit_message').innerHTML = "";
 
@@ -41,16 +49,24 @@ async function submitRsvp(event) {
 
 
     if (validHashes.includes(hashedCode)) {
+        document.getElementById('rsvpDetailsForm').style.display = 'none';
+        document.getElementById("fh5co-rsvp").scrollIntoView({ behavior: "smooth", block: "start" });
+        document.getElementById("fh5co-rsvp").focus();
+        document.getElementById('response_message').innerHTML = "<strong>Submitting...</strong> Please don't leave this page.";
+        document.getElementById('retrieve_invite').style.display = 'inline';
+            
         var response = await postSheetsApi(inviteCodeInput, data);
         // console.log( );
         if (response.result = "success") {
             // window.onload = function() {
             // };
-            document.getElementById('rsvpDetailsForm').style.display = 'none';
-            document.getElementById("fh5co-rsvp").scrollIntoView({ behavior: "smooth", block: "start" });
-            document.getElementById("fh5co-rsvp").focus();
             document.getElementById('response_message').innerHTML = '<strong>Submitted!</strong> Your response has been recorded.';
-            document.getElementById('retrieve_invite').style.display = 'inline';
+            if (document.getElementById('rsvp_yes').className.includes('active')) {
+                document.getElementById('rsvp_yes_calendar').style.display = 'inline';
+            }
+            document.getElementById('rsvpDetailsForm').reset();
+            document.getElementById('rsvp-form').reset();
+            document.querySelectorAll('.toggle-button').forEach(btn => btn.classList.remove('active'));
         }
         else {
             document.getElementById('submit_message').innerHTML = '<strong>Error!</strong> Your response has not been recorded. Try again soon.';
@@ -79,11 +95,11 @@ async function getSheetsApi(inviteCodeInput) {
     // document.getElementById('information_message').innerHTML = jsonData;
     if (jsonData.formal_name.toString() !== "") {
         document.getElementById('information_message').innerHTML = 
-        `${jsonData.formal_name.toString()}<br>` +
-        `Welcome to Our Wedding<br>` + 
-        `It is our honor to invite you, along with the esteemed guest(s) listed below, to share in this special day with us.<br>` +
-        `Kindly indicate the guest(s) for whom you are responding. If desired, enter their contact information to receive event updates.<br>`;
-        document.getElementById('information_message').style.display = 'inline-flex';
+        `<h2>${jsonData.formal_name.toString()},<br></h2>` +
+        `<h3>Welcome to Our Wedding<br></h3>` + 
+        `<div class="col-md-8 col-md-offset-2"><p>It is our honor to invite you, along with the esteemed guest(s) listed below, to share in this special day with us. Select your RSVP response below.<br>&#8212;<br>` +
+        `Kindly indicate the guest(s) for whom you are responding. If desired, enter their contact information to receive event updates.<br></p>&#8595</div>`;
+        document.getElementById('information_message').style.display = 'inline';
         document.getElementById('full_name').style.display = 'inline-flex';
         document.getElementById('email').style.display = 'inline-flex';
         document.getElementById('phone').style.display = 'inline-flex';
@@ -119,7 +135,7 @@ async function getSheetsApi(inviteCodeInput) {
 
 async function getData(inviteCodeInput) {
     try {
-      const response = await fetch(`https://script.google.com/macros/s/AKfycbzDJOYvfamZNkpF1aqGIA2AS9UFYBmdZ8GgpEpUxoYeRhWYp6DfTLElBHCWuscjOcTn9A/exec?invite_code=${inviteCodeInput.toString()}`);
+      const response = await fetch(`https://script.google.com/macros/s/AKfycbyIxbUTQmaqFIRzTCkpBYBkGYSLNPjLxxcb0z-rzGaDiTc3nK_v_aMSqdH_gUbd24pQ/exec?invite_code=${inviteCodeInput.toString()}`);
       if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
       }
@@ -132,13 +148,24 @@ async function getData(inviteCodeInput) {
   }
 
 
-async function toggleRequiredField() {
-    var radioYes = document.getElementById('rsvp_yes');
+async function toggleRequiredField(button) {
+    // Prevent form from submitting on click
+    event.preventDefault();
+
+    // Toggle selection
+    let buttons = document.querySelectorAll('.toggle-button');
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    // Update rsvp input value
+    document.getElementById('rsvp').value = button.getAttribute('value');
+    
+    var rsvp_yes = document.getElementById('rsvp_yes');
     // var inputField1 = document.getElementById('full_name');
     var inputField = document.getElementById('phone');
 
     // If "Yes" is selected, make the input field required
-    if (radioYes.checked) {
+    if (rsvp_yes.className.includes('active')) {
         inputField.required = true;
         // inputField2.required = true;
     } else {
@@ -153,14 +180,14 @@ function showForm(checkbox) {
     var id = checkbox.id.substring(checkbox.id.length-1);
     if (document.getElementById(checkbox.id).checked) {
         checkboxFormId.style.display = 'block';
-        if (document.querySelector(`label[for="invited_${id}"]`).innerText == "+1") {
+        if (document.querySelector(`label[for="invited_${id}"]`).innerText == "+1 guest") {
             document.getElementById("name" + id).required = true;
             document.getElementById("name" + id).style.display = 'inline';
         }
     }
     else {
         checkboxFormId.style.display = 'none';
-        if (document.querySelector(`label[for="invited_${id}"]`).innerText == "+1") {
+        if (document.querySelector(`label[for="invited_${id}"]`).innerText == "+1 guest") {
             document.getElementById("name" + id).required = false;
             document.getElementById("name" + id).style.display = 'none';
         }
@@ -171,7 +198,7 @@ function showForm(checkbox) {
 async function postSheetsApi(inviteCodeInput, data) {
     const queryString = new URLSearchParams(data).toString();
     try {
-        const response = await fetch(`https://script.google.com/macros/s/AKfycbzDJOYvfamZNkpF1aqGIA2AS9UFYBmdZ8GgpEpUxoYeRhWYp6DfTLElBHCWuscjOcTn9A/exec?${queryString}`, {
+        const response = await fetch(`https://script.google.com/macros/s/AKfycbyIxbUTQmaqFIRzTCkpBYBkGYSLNPjLxxcb0z-rzGaDiTc3nK_v_aMSqdH_gUbd24pQ/exec?${queryString}`, {
             method: 'POST',
         });
 
